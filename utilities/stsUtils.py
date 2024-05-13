a2d65c6e6250492f3e16ef085f28e47f19ccffda
lovelife_lizhe <513278236@qq.com>
2024-05-13 14:40:08 +0800

    all path get done

--- utilities/stsUtils.py
+++ utilities/stsUtils.py
@@ -1,34 +1,66 @@
 import json
 
 
+def find_paths(start_nodes, all_nodes):
+    paths = []
+    current_path = []
+
+    def dfs(node):
+        current_path.append(node)
+        if node['y'] == 16:
+            paths.append(list(current_path))
+        else:
+            for child in node['children']:
+                dfs(getNodeByXY(child['x'], child['y'], all_nodes))
+
+        current_path.pop()
+
+    for start_node in start_nodes:
+        dfs(start_node)
+
+    return paths
+
+
 def getNodeByXY(x, y, all_nodes):
     for node in all_nodes:
         if node['x'] == x and node['y'] == y:
             return node
 
 
-def getPath(start_node, cur_path, all_nodes):
-    for pos in start_node['children']:
-        print(pos['x'], pos['y'])
-        node = getNodeByXY(pos['x'], pos['y'], all_nodes)
-        if pos['y'] == 16:
-            return cur_path + [node]
-        else:
-            return getPath(node, cur_path + [node], all_nodes)
-
-
-def getAllPaths():
-    with open('../tmp.json', encoding='utf-8') as f:
-        game_state = json.load(f)
-    sts_map = game_state['game_state']['map']
-    # get all node with y = 0
-    start_nodes = []
+class GetPaths:
     paths = []
-    for node in sts_map:
-        if node['y'] == 0:
-            paths += getPath(node, [node], sts_map)
-    print(paths)
+
+    def getPath(self, start_node, cur_path, all_nodes):
+        for pos in start_node['children']:
+            print(pos['x'], pos['y'])
+            node = getNodeByXY(pos['x'], pos['y'], all_nodes)
+            if pos['y'] == 16:
+                self.paths.append(cur_path + [node])
+            else:
+                return self.getPath(node, (cur_path + [node]).copy(), all_nodes)
+
+    def getAllPaths(self):
+        with open('../tmp.json', encoding='utf-8') as f:
+            game_state = json.load(f)
+        end_node = {
+            "symbol": "B",
+            "phrase": "INCOMPLETE",
+            "children": [],
+            "x": 3,
+            "y": 16,
+            "hasEmeraldKey": False,
+            "parents": []
+        }
+        sts_map = game_state['game_state']['map']
+        sts_map.append(end_node)
+        # get all node with y = 0
+        start_nodes = [node for node in sts_map if node['y'] == 0]
+        self.paths = find_paths(start_nodes, sts_map)
+
+        return self.paths
 
 
 if __name__ == '__main__':
-    getAllPaths()
+    k = GetPaths()
+    paths = k.getAllPaths()
+    print(paths)
